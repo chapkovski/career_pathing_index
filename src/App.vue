2c36811124d782f94cc0cc6086af891db9583567
Philipp Chapkovski <chapkovski@gmail.com>
2025-07-08 05:59:05 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <SurveyComponent :model="survey" />
+</template>
+<script setup lang="ts">
+    import { Model } from "survey-core";
+    import { SurveyComponent } from "survey-vue3-ui";
+    import "./index.css";
+    import { json } from "./json";
+
+    const survey = new Model(json);
+    survey.onComplete.add((sender, options) => {
+        console.log(JSON.stringify(sender.data, null, 3));
+    });
+</script>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "survey-core/survey-core.min.css";
+
+const app = createApp(App);
+app.mount("#app");
